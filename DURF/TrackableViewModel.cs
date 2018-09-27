b47af077761b67171a7fb28d41c7b29a5a54ce0d
Brent Bulla <brent.bulla.jr@gmail.com>
2018-09-27 11:28:12 -0400

    comments plus make Onpropertychanging block change, if inheriting class returns false.

--- DURF/TrackableViewModel.cs
+++ DURF/TrackableViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using DURF.Collections;
@@ -9,15 +10,14 @@ namespace DURF
     /// <summary>
     /// ViewModel base class that raise prop changed using convenient Get/Set methods and tracks property changes
     /// </summary>
+    [Serializable]
     public abstract class TrackableViewModel : INotifyPropertyChanged, IViewStateAware
     {
-        #region Fields
         private Dictionary<string, object> _propMap = null;
-        #endregion
 
         protected virtual bool TrackChanges { get; } = true;
 
-        protected T Get<T>(T defaultValue, [CallerMemberName] string name = null)
+        protected virtual T Get<T>(T defaultValue, [CallerMemberName] string name = null)
         {
             if (!PropertyMap.ContainsKey(name))
                 return defaultValue;
@@ -28,7 +28,7 @@ namespace DURF
             return (T)result;
         }
 
-        protected T Get<T>([CallerMemberName] string propName = null)
+        protected virtual T Get<T>([CallerMemberName] string propName = null)
         {
             return this.Get<T>(default(T), propName);
         }
@@ -39,10 +39,9 @@ namespace DURF
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="value"></param>
-        /// <param name="propName"></param>
-        /// <param name="nonSerialized"></param>
+        /// <param name="name"></param>
         /// <param name="raisePropChanged"></param>
-        /// <returns></returns>
+        /// <returns>True if property was changed (value was different).  False if not</returns>
         protected virtual bool Set<T>(T value, [CallerMemberName] string name = null, bool raisePropChanged = true)
         {
             T currentValue = default(T);
@@ -57,13 +56,16 @@ namespace DURF
                     if (currentValue != null && currentValue.Equals(value))
                         return false;
 
-                    PropertyMap[name] = value;
+                    if (OnPropertyChanging(name, currentValue, raisePropChanged))
+                        PropertyMap[name] = value;
+                    else return false;
                 }
-                else
+                else if (OnPropertyChanging(name, currentValue, raisePropChanged))
                     PropertyMap.Add(name, value);
+                else return false;
             }
 
-            OnPropertyChanging(name, currentValue, raisePropChanged);
+
 
             if (raisePropChanged)
                 RaisePropertyChanged(name);
@@ -71,27 +73,30 @@ namespace DURF
             return true;
         }
 
-        private Dictionary<string, object> PropertyMap => _propMap ?? (_propMap = new Dictionary<string, object>());
+        protected virtual Dictionary<string, object> PropertyMap => _propMap ?? (_propMap = new Dictionary<string, object>());
 
 
         /// <inheritdoc />
+        [field: NonSerialized]
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void RaisePropertyChanged([CallerMemberName] string name = null)
+        protected virtual void RaisePropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
         /// <summary>
-        /// Called right before the PropertyChanged event is fired
+        /// Called right before the PropertyChanged event is fired. Return false to block the change
         /// </summary>
         /// <param name="name"></param>
         /// <param name="currentValue">value before the change</param>
-        /// <param name="raiseEvent">whether or not the event will actually be raised</param>
-        protected virtual void OnPropertyChanging(string name, object currentValue, bool raiseEvent)
+        /// <param name="raiseEvent">whether or not the PropertyChanged event will actually be raised</param>
+        protected virtual bool OnPropertyChanging(string name, object currentValue, bool raiseEvent)
         {
             if(TrackChanges)
                 TrackableScope.Current?.TrackChange(() => Set(currentValue, name, raiseEvent));
+
+            return true;
         }
 
         #region Implementation of IViewStateAware
